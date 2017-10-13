807c59ecdca877a8cd7296d6c31998f08f9b354d
椎野大地 <shiinodaichi@gmail.com>
2017-10-14 05:12:37 +0900

    ドラクエ風課題

---
+++ dq風課題 3/functions1.js
@@ -0,0 +1,24 @@
+function sound(){
+	document.getElementById( 'sound-file' ).play();
+};
+		// [ID:sound-file]の音声ファイルを再生[play()]する
+		//<sound id="sound-file" src=""></sound>
+
+//呪い効果音を再生するnoroi()
+		function noroi(){
+		document.getElementById('noroi').play();
+};
+
+//バトルBGMを再生
+function battle(){
+		document.getElementById('battle').play();
+	
+	function jumon(){
+			document.getElementById('jumon').play();//呪文サウンド
+			
+	}
+	
+	function damage(){
+		document.getElementById('damage').play();
+	}
+	
